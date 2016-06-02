666cf9c906c6b1203c01c08f9bdbf51f1a492203
Crt <crt@ibzoot.com>
2016-06-02 14:13:49 +0200

    added server config

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /app/NetworkBase/src/NetworkBase
 sudo dotnet restore
+if [ -d /app/server/ ]; then
+	cd /app/server/src/ServerTest2/
+	sudo dotnet restore
+	sudo dotnet run --configuration Release
+fi
+
 if [ -d /app/client/ ]; then
 	cd /app/client/src/ConsoleApp1/
 	sudo dotnet restore
