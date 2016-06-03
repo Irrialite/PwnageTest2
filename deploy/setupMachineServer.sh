44e62ffc6c047f1fefa39d49d0cada81c7376658
Crt <crt@ibzoot.com>
2016-06-03 10:21:19 +0200

    added and fixed scripts

---
+++ deploy/setupMachineServer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+curl -O https://bootstrap.pypa.io/get-pip.py
+python2.7 get-pip.py
+pip install awscli
+pip install --upgrade awscli
+apt-get -y update
+apt-get -y install ruby2.0
+apt-get -y install wget
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+/opt/codedeploy-agent/bin/install auto
+
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+apt-get -y update
+apt-get -y install dotnet-dev-1.0.0-preview1-002702
+
+# nginx stuff
+apt-get -y install nginx
+cat <<EOM >"/etc/nginx/sites-available/default"
+server {
+	listen 80;
+	
+	server_name localhost;
+	location / {
+		proxy_pass http://localhost:5004;
+		proxy_http_version 1.1;
+		proxy_set_header Upgrade $http_upgrade;
+		proxy_set_header Connection keep-alive;
+		proxy_set_header Host $host;
+		proxy_cache_bypass $http_upgrade;
+	}
+}
+EOM
+nginx -s reload
--- deploy/start.sh
+++ deploy/start.sh
@@ -4,11 +4,17 @@ sudo dotnet restore
 if [ -d /app/server/ ]; then
 	cd /app/server/src/ServerTest2/
 	sudo dotnet restore
-	sudo dotnet run --configuration Release
+	path=$(sudo dotnet publish --configuration Release | grep -Pio "(?<=published to )(.+)")
+	cd $path
+	# Redirect stdout, stderr, and stdin to dev null to let aws continue deployment
+	sudo dotnet ServerTest2.dll > /dev/null 2> /dev/null < /dev/null &
 fi
 
 if [ -d /app/client/ ]; then
 	cd /app/client/src/ConsoleApp1/
 	sudo dotnet restore
-	sudo dotnet run --configuration Release
+	path=$(sudo dotnet publish --configuration Release | grep -Pio "(?<=published to )(.+)")
+	cd $path
+	# Redirect stdout, stderr, and stdin to dev null to let aws continue deployment
+	sudo dotnet ConsoleApp1.dll > /dev/null 2> /dev/null < /dev/null &
 fi
